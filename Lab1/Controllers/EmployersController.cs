32defd87f33eb3979291f62b55e388655fefafa7
Cole Anderson <coleanderson732@gmail.com>
2024-10-24 18:14:08 -0400

    added links to all controllers in layout, and changed privacy page

--- Lab1/Controllers/EmployersController.cs
+++ Lab1/Controllers/EmployersController.cs
@@ -13,6 +13,14 @@ using Microsoft.AspNetCore.Authorization;
 // original work. No other person's work has been used without due
 // acknowledgement and I have not made my work available to anyone else.
 
+// I, Ryan Gold, student number 000855957, certify that this material is my
+// original work. No other person's work has been used without due
+// acknowledgement and I have not made my work available to anyone else.
+
+// I, Tristan Cerillo, student number 000843061, certify that this material is my
+// original work. No other person's work has been used without due
+// acknowledgement and I have not made my work available to anyone else.
+
 namespace Lab1.Controllers
 {
     /// <summary>
