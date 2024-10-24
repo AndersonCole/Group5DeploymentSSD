1297501cb0ccc9a9ab2589e9d5f848ae8bade6a1
Cole Anderson <coleanderson732@gmail.com>
2024-10-24 17:56:40 -0400

    added model for work order

--- Lab1/Models/WorkOrder.cs
+++ Lab1/Models/WorkOrder.cs
@@ -5,5 +5,22 @@ namespace Lab1.Models
     public class WorkOrder
     {
 
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Employee Name")]
+        public string EmployeeName { get; set; }
+
+        [Required]
+        [Display(Name = "Customer")]
+        public string Customer { get; set; }
+        [Required]
+        [Display(Name = "Work Description")]
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Date")]
+        public DateTime? Date { get; set; }
     }
 }
