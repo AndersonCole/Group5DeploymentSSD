b2c4143199cc9b96b60501fdacb7c8309f9d765d
Cole Anderson <coleanderson732@gmail.com>
2024-10-24 17:51:04 -0400

    re scaffolded cars to be plural

--- Lab1/Controllers/CarsController.cs
+++ Lab1/Controllers/CarsController.cs
@@ -22,20 +22,20 @@ namespace Lab1.Controllers
         // GET: Cars
         public async Task<IActionResult> Index()
         {
-              return _context.Car != null ? 
-                          View(await _context.Car.ToListAsync()) :
-                          Problem("Entity set 'ApplicationDbContext.Car'  is null.");
+              return _context.Cars != null ? 
+                          View(await _context.Cars.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Cars'  is null.");
         }
 
         // GET: Cars/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null || _context.Car == null)
+            if (id == null || _context.Cars == null)
             {
                 return NotFound();
             }
 
-            var car = await _context.Car
+            var car = await _context.Cars
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (car == null)
             {
@@ -70,12 +70,12 @@ namespace Lab1.Controllers
         // GET: Cars/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null || _context.Car == null)
+            if (id == null || _context.Cars == null)
             {
                 return NotFound();
             }
 
-            var car = await _context.Car.FindAsync(id);
+            var car = await _context.Cars.FindAsync(id);
             if (car == null)
             {
                 return NotFound();
@@ -121,12 +121,12 @@ namespace Lab1.Controllers
         // GET: Cars/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null || _context.Car == null)
+            if (id == null || _context.Cars == null)
             {
                 return NotFound();
             }
 
-            var car = await _context.Car
+            var car = await _context.Cars
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (car == null)
             {
@@ -141,14 +141,14 @@ namespace Lab1.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            if (_context.Car == null)
+            if (_context.Cars == null)
             {
-                return Problem("Entity set 'ApplicationDbContext.Car'  is null.");
+                return Problem("Entity set 'ApplicationDbContext.Cars'  is null.");
             }
-            var car = await _context.Car.FindAsync(id);
+            var car = await _context.Cars.FindAsync(id);
             if (car != null)
             {
-                _context.Car.Remove(car);
+                _context.Cars.Remove(car);
             }
             
             await _context.SaveChangesAsync();
@@ -157,7 +157,7 @@ namespace Lab1.Controllers
 
         private bool CarExists(int id)
         {
-          return (_context.Car?.Any(e => e.Id == id)).GetValueOrDefault();
+          return (_context.Cars?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
--- Lab1/Models/WorkOrder.cs
+++ Lab1/Models/WorkOrder.cs
@@ -1,4 +1,6 @@
-﻿namespace Lab1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab1.Models
 {
     public class WorkOrder
     {
