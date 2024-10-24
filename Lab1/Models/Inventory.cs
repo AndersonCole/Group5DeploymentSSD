216dfc7048d33412ab6b761b8859764736a97e71
RJGold <158121677+RJGold@users.noreply.github.com>
2024-10-24 17:43:53 -0400

    Completed Inventory model

--- Lab1/Models/Inventory.cs
+++ Lab1/Models/Inventory.cs
@@ -1,6 +1,25 @@
-﻿namespace Lab1.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab1.Models
 {
     public class Inventory
     {
+        [Key]
+        [Display(Name="Inventory ID")]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name="Item Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name="Quantity In Stock")]
+        public int Quantity { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name="Date Last Ordered")]
+        public DateTime? LastOrderDate { get; set; }
     }
 }
